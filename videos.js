307f42e7aebe5365b88c5f842a43d33cbc62f729
parvez <parvezaahmed98@gmail.com>
2017-11-03 10:50:57 +0530

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,238 @@
+{ 
+    "_id" : ObjectId("59eb5cce4dc9d31875ad229c"), 
+    "name" : "x-man", 
+    "url" : "/x-man.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb5cce4dc9d31875ad229d"), 
+    "name" : "spider-man", 
+    "url" : "/spider-man.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb5db74dc9d31875ad229e"), 
+    "name" : "spider-man2", 
+    "url" : "/spider-man2.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb5db74dc9d31875ad229f"), 
+    "name" : "ice-age", 
+    "url" : "/ice-age.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42268"), 
+    "name" : "angles-demons", 
+    "url" : "/angles-demons.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42269"), 
+    "name" : "eagle-eye", 
+    "url" : "/eagle-eye.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226a"), 
+    "name" : "gladiator", 
+    "url" : "/gladiator.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226b"), 
+    "name" : "house", 
+    "url" : "/house.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226c"), 
+    "name" : "magneto", 
+    "url" : "/magneto.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226d"), 
+    "name" : "motive", 
+    "url" : "/motives.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226e"), 
+    "name" : "narnia", 
+    "url" : "/narnia.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b4226f"), 
+    "name" : "oculus", 
+    "url" : "/oculus.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42270"), 
+    "name" : "the-kingdom", 
+    "url" : "/the-kingdom.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42271"), 
+    "name" : "the-prestige", 
+    "url" : "/the-prestige.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42272"), 
+    "name" : "transformers", 
+    "url" : "/transformers.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42273"), 
+    "name" : "valkrie", 
+    "url" : "/valkrie.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6713e143d79fe1b42274"), 
+    "name" : "vacancy", 
+    "url" : "/vacancy.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e32"), 
+    "name" : "x-man", 
+    "url" : "/x-man.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e33"), 
+    "name" : "spider-man", 
+    "url" : "/spider-man.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e34"), 
+    "name" : "spider-man2", 
+    "url" : "/spider-man2.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e35"), 
+    "name" : "ice-age", 
+    "url" : "/ice-age.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e36"), 
+    "name" : "angles-demons", 
+    "url" : "/angles-demons.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e37"), 
+    "name" : "eagle-eye", 
+    "url" : "/eagle-eye.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e38"), 
+    "name" : "gladiator", 
+    "url" : "/gladiator.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e39"), 
+    "name" : "house", 
+    "url" : "/house.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3a"), 
+    "name" : "magneto", 
+    "url" : "/magneto.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3b"), 
+    "name" : "motive", 
+    "url" : "/motives.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3c"), 
+    "name" : "narnia", 
+    "url" : "/narnia.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3d"), 
+    "name" : "oculus", 
+    "url" : "/oculus.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3e"), 
+    "name" : "the-kingdom", 
+    "url" : "/the-kingdom.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e3f"), 
+    "name" : "the-prestige", 
+    "url" : "/the-prestige.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e40"), 
+    "name" : "transformers", 
+    "url" : "/transformers.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e41"), 
+    "name" : "valkrie", 
+    "url" : "/valkrie.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
+{ 
+    "_id" : ObjectId("59eb6b6fe5ee4d3811718e42"), 
+    "name" : "vacancy", 
+    "url" : "/vacancy.jpg", 
+    "like" : 0.0, 
+    "dislike" : 0.0
+}
